0a8fa6576c2da218f57684ed1c4e3c2418b263ce
Darryl Ross <darryl@afoyi.com>
2014-06-25 16:17:25 +0930

    Update setup.py to include templates and static media.

--- setup.py
+++ setup.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python
 
-import os.path
 from glob import glob
-from distutils.core import setup
+from setuptools import setup
 
 import versioneer
 versioneer.VCS = 'git'
@@ -11,18 +10,19 @@ versioneer.versionfile_build = 'netman/_version.py'
 versioneer.tag_prefix = 'v'
 versioneer.parentdir_prefix = 'django-netman-'
 
-packages = [ 'netman', 'netman/ipaddr' ]
-media_files = [ 'netman/static/css/*.css', 'netman/templates/*.html', 'netman/templates/*/*.html' ]
-package_data = dict( (package_name, media_files) for package_name in packages )
 
 setup(
     name='django-netman',
+    cmdclass=versioneer.get_cmdclass(),
     version=versioneer.get_version(),
     description='Network management modules for Django',
     author='Darryl Ross',
     author_email='darryl@afoyi.com',
     url='https://git.afoyi.com/django-modules/django-netman/',
-    packages=packages,
-    package_data=package_data,
-    cmdclass=versioneer.get_cmdclass()
+    packages=[ 'netman', 'netman/ipaddr' ],
+    package_data={
+        'netman': glob('netman/static/css/*.css') + glob('netman/templates/*.html'),
+        'netman/ipaddr': glob('netman/templates/ipaddr/*.html')
+    },
+    include_package_data=True
 )
