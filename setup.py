98b2fa42346c515b2977d31b87ce8e9d6764fcc5
Darryl Ross <darryl@afoyi.com>
2014-06-25 10:16:51 +0930

    Add setup.py with versioneer support.

---
+++ setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+import os.path
+from glob import glob
+from distutils.core import setup
+
+import versioneer
+versioneer.VCS = 'git'
+versioneer.versionfile_source = 'netman/_version.py'
+versioneer.versionfile_build = '_version.py'
+versioneer.tag_prefix = 'v'
+versioneer.parentdir_prefix = 'django-netman-'
+
+
+setup(
+	name='django-netman',
+	version=versioneer.get_version(),
+	description='Network management modules for Django',
+	author='Darryl Ross',
+	author_email='darryl@afoyi.com',
+	url='https://git.afoyi.com/django-modules/django-netman/',
+    packages=['netman'],
+    cmdclass=versioneer.get_cmdclass()
+)
